550a0d8572e799c02e10911b1035a28290917cfb
Orange <as812019@student.nitw.ac.in>
2023-02-11 16:58:36 +0530

    gets top 5 for all inputs

--- serve.py
+++ serve.py
@@ -1,12 +1,23 @@
 from flask import Flask, jsonify, request
+import requests
 import pickle
 import json
+import pandas as pd
+
+URL = 'http://api.weatherapi.com/v1/future.json'
+APIKEY = '49937b2e2ca847928e150828231102'
+# &q=Islampur&dt=2023-03-13
 
 
 grad = pickle.load(open('local.sav', 'rb'))
-head = ['N', 'P', 'K', 'temp', 'humid', 'ph', 'rain']
+head = ['N', 'P', 'K', 'temperature', 'humidity', 'ph', 'rainfall']
 targets = json.loads(open('map.dict').read())
-print(targets)
+
+
+df = pd.read_csv('rainfall.csv')
+dist = dict(list(df.groupby('STATE_UT_NAME')['DISTRICT']))
+rainfall = df[['DISTRICT', 'ANNUAL']].set_index('DISTRICT').to_dict()['ANNUAL']
+
 
 app = Flask(__name__)
 
@@ -19,36 +30,77 @@ def home():
 
 
 # query the neural network
-@app.route('/query/', methods = ['POST'])
-def disp():
+@app.route('/queryALL/', methods = ['POST'])
+def queryALL():
 	n = request.form['N']
 	p = request.form['P']
 	k = request.form['K']
 	temp = request.form['temp']
 	humid = request.form['humid']
 	ph = request.form['ph']
-	rain = request.form['rain']
+	rain = request.form['rain'] # average annual rainfall
 
+	X = [[n, p, k, temp, humid, ph, rain]]
+	x = pd.DataFrame(X, columns=head)
+
+	prob = grad.predict_proba(x)[0]
+	p = [[targets[str(i)], prob[i]] for i in range(len(prob))]
+	p = sorted(p, key=lambda x: x[1], reverse=True)
+
+	return jsonify({'predicted': p[0], 'next 4': p[1:5]})
+
+@app.route('/queryLD/', methods = ['POST'])
+def queryLD():
+	district = request.form['district']
+	state = request.form['state']
+	month = request.form['month']
+	year = request.form['year']
+	n = request.form['N']
+	p = request.form['P']
+	k = request.form['K']
+	ph = request.form['ph']
+
+	month = str(month)
+	if(len(month) == 1):
+		month = '0' + month
+	rain = rainfall[district.upper()] * 0.0393701
+	
+	w = getWeather(location = district, year = year, month = month)
+	temp = w['avgtemp']
+	humid = w['avghum']
 	X = [n, p, k, temp, humid, ph, rain]
+	print(X)
 
 	res = targets[str(grad.predict([X])[0])]
-	
 	return jsonify({'predicted': res})
 
-# @app.route('/query2/', methods = ['POST'])
-# def disp():
-# 	n = request.form['location']
-# 	p = request.form['month']
 
+def getWeather(location, year, month):
+	ym = str(year) + '-' + str(month) + '-'
+	params = {
+		'key': APIKEY,
+		'q'  : location,
+		'dt' : ym + '01'
+	}
+	res = []
+	r = requests.get(url = URL, params = params)
+	if(r.status_code != 200):
+		print(r.json())
+		raise Exception("Somethings wrong cant get from weather API")
+	res.append(r.json())
+	params['dt'] = ym + '15'
+	res.append(requests.get(url = URL, params = params).json())
+	params['dt'] = ym + '28'
+	res.append(requests.get(url = URL, params = params).json())
 
-	# X = [n, p, k, temp, humid, ph, rain]
+	t = [r['forecast']['forecastday'][0]['day']['avgtemp_c'] for r in res]
+	h = [r['forecast']['forecastday'][0]['day']['avghumidity'] for r in res]
+	result = {
+		'avgtemp' : sum(t) / 3,
+		'avghum' : sum(h) / 3
+	}
+	return result
 
-	# res = targets[str(grad.predict([X])[0])]
-	
-	# return jsonify({'predicted': res})
-
-
-# driver function
 if __name__ == '__main__':
 
 	app.run(debug = True)
